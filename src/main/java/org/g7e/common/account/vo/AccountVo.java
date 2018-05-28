5d61b2af228ae5f15c02229bc9e1a4f699bb3b0b
westjoe <lingzg@126.com>
2018-05-28 22:50:17 +0800

    3.fixed config

--- src/main/java/org/g7e/common/account/vo/AccountVo.java
+++ src/main/java/org/g7e/common/account/vo/AccountVo.java
@@ -1,5 +1,143 @@
 package org.g7e.common.account.vo;
 
 public class AccountVo {
+	private Integer id;
 
+	private String username;
+
+	private String password;
+
+	private String passwordSalt;
+
+	private Boolean gender;
+
+	private Integer birthday;
+
+	private Integer registerTime;
+
+	private Integer lastLoginTime;
+
+	private String lastLoginIp;
+
+	private String nickname;
+
+	private String mobile;
+
+	private String registerIp;
+
+	private String avatar;
+
+	private Integer isDelete;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPasswordSalt() {
+		return passwordSalt;
+	}
+
+	public void setPasswordSalt(String passwordSalt) {
+		this.passwordSalt = passwordSalt;
+	}
+
+	public Boolean getGender() {
+		return gender;
+	}
+
+	public void setGender(Boolean gender) {
+		this.gender = gender;
+	}
+
+	public Integer getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Integer birthday) {
+		this.birthday = birthday;
+	}
+
+	public Integer getRegisterTime() {
+		return registerTime;
+	}
+
+	public void setRegisterTime(Integer registerTime) {
+		this.registerTime = registerTime;
+	}
+
+	public Integer getLastLoginTime() {
+		return lastLoginTime;
+	}
+
+	public void setLastLoginTime(Integer lastLoginTime) {
+		this.lastLoginTime = lastLoginTime;
+	}
+
+	public String getLastLoginIp() {
+		return lastLoginIp;
+	}
+
+	public void setLastLoginIp(String lastLoginIp) {
+		this.lastLoginIp = lastLoginIp;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getRegisterIp() {
+		return registerIp;
+	}
+
+	public void setRegisterIp(String registerIp) {
+		this.registerIp = registerIp;
+	}
+
+	public String getAvatar() {
+		return avatar;
+	}
+
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+
+	public Integer getIsDelete() {
+		return isDelete;
+	}
+
+	public void setIsDelete(Integer isDelete) {
+		this.isDelete = isDelete;
+	}
 }
